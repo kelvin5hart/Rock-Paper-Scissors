d6dd70a656b3149fe06d209bf1891fa43279ccb0
Kelvin Atemie-Hart <78157233+kelvin5hart@users.noreply.github.com>
2022-01-21 06:14:50 +0000

    Print you loose if player enters an invalid number

--- main.py
+++ main.py
@@ -30,22 +30,22 @@ scissors = '''
 #Write your code below this line 👇
 
 player = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
-computer = random.randint(0, 2)
+
 choice = [rock, paper, scissors]
 
-if player < 3:
+if player < 3 and player >= 0:
+  computer = random.randint(0, 2)
   print(f"You chose:\n {choice[player]}")
-else:
-  print("You entered an invalid reponse")
-print(f"computer chose:\n {choice[computer]}")
+  print(f"computer chose:\n {choice[computer]}")
 
-if computer == player:
-  print("Draw")
-elif (computer == 0 and player == 2) or (computer == 1 and player == 0) or (computer == 2 and player == 1):
-  print("Computer Wins, You loose!")
-elif (player == 0 and computer == 2) or (player == 1 and computer == 0) or (player == 2 and computer == 1):
-  print("You Won!")
+  if computer == player:
+    print("Draw")
+  elif (computer == 0 and player == 2) or (computer == 1 and player == 0) or (computer == 2 and player == 1):
+    print("Computer Wins, You loose!")
+  elif (player == 0 and computer == 2) or (player == 1 and computer == 0) or (player == 2 and computer == 1):
+    print("You Won!")
 else:
-  print("You loose!")
+  print("You entered an invalid reponse, You Loose!")
+
 
 
